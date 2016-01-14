2ad2ba5a92bffebf947084ad48c2c759267f6e16
Kersten Burkhardt <kerstenk@gmail.com>
2016-01-14 22:50:52 +0100

    use archer instead of enum, because enum is not standard SQL

--- ext_tables.sql
+++ ext_tables.sql
@@ -91,7 +91,7 @@ CREATE TABLE tx_yag_domain_model_item (
 	title tinytext,
 	description text,
 	href tinytext,
-	href_target enum('', '_blank', '_self') DEFAULT '' NOT NULL,
+	href_target varchar(32) DEFAULT '' NOT NULL,
 	tags int(11) unsigned DEFAULT '0' NOT NULL,
 	
 	filename varchar(200) DEFAULT '' NOT NULL,
